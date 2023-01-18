afbd242e085711126b3b7764da7a61edc8cc7cd0
Light-JH <phoebehu@outlook.com>
2023-01-17 16:00:16 -0800

    Add signal handling to main

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
+#include <signal.h>
+#include <stdio.h>
+
+static volatile int receivedSigInt = 0;
+
+void intHandler(int signal)
+{
+    receivedSigInt = 1;
+}
+
 int main(int argc, char **argv)
 {
+    // Regist SIGINT handling
+    signal(SIGINT, intHandler);
+
+    while (!receivedSigInt)
+    {
+    }
+
+    printf("Exiting...\n");
+
     return 0;
 }
